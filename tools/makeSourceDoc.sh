ce6f77c070df49fc50ec0b9e35db46fed68cea86
Arpad Magosanyi <m4gw4s@gmail.com>
2025-07-20 17:12:53 +0200

    pdf and docbook gets to site, README prettier, source attached

--- tools/makeSourceDoc.sh
+++ tools/makeSourceDoc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -xe
 TOOLDIR=$(dirname $0)
-mkdir -p target/site target/generated-site/markdown
+mkdir -p target/generated-site/markdown target/generated-site/resources
 java -cp $($TOOLDIR/mkClassPath.sh) io.github.magwas.tooling.Main . >target/AST.xml
-$TOOLDIR/makeSourceDocbook.sh . > target/site/apidoc.docbook
-pandoc -s -o target/generated-site/markdown/apidoc.md -f docbook target/site/apidoc.docbook
-pandoc -s --toc -o target/generated-site/apidoc.pdf -f docbook target/site/apidoc.docbook
+$TOOLDIR/makeSourceDocbook.sh . > target/generated-site/resources/apidoc.docbook
+pandoc -s -o target/generated-site/markdown/apidoc.md -f docbook target/generated-site/resources/apidoc.docbook
+pandoc -s --toc -o target/generated-site/resources/apidoc.pdf -f docbook target/generated-site/resources/apidoc.docbook
+cp README target/generated-site/markdown/index.md
