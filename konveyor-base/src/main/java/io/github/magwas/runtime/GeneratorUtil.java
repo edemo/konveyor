54f4e8617001fd21192263198a52e93a0de2a648
gbq6 <24852447+gbq6@users.noreply.github.com.>
2025-09-21 01:38:46 +0200

    Applied suggested changes #77

--- konveyor-base/src/main/java/io/github/magwas/runtime/GeneratorUtil.java
+++ konveyor-base/src/main/java/io/github/magwas/runtime/GeneratorUtil.java
@@ -14,34 +14,32 @@ public class GeneratorUtil {
 		String klassName = fullClassName.replaceFirst(".*\\.", "")
 				.replace("Generator", "");
 		String packageName = fullClassName.replaceFirst("\\.[^.]*$", "");
-		builder.append("package ");
-		builder.append(packageName);
-		builder.append(";\n");
-		builder.append(preamble);
-		builder.append("\n");
-		builder.append("public interface ");
-		builder.append(klassName);
+		builder.append("package ")
+				.append(packageName)
+				.append(";\n")
+				.append(preamble)
+				.append("\npublic interface ")
+				.append(klassName);
 		if (extendeds.length != 0) {
-			builder.append(" extends ");
-			builder.append(String.join(", ", extendeds));
+			builder.append(" extends ").append(String.join(", ", extendeds));
 		}
 		builder.append(" {\n");
 	}
 
 	public static void mapToCode(String input,
-			Function<? super String, ? extends String> mapper,
+			Function<? super String, String> mapper,
 			StringBuilder builder) {
 		linesOf(input).map(mapper).forEach(builder::append);
 	}
 
 	public static void mapToCode(Stream<String> input,
-			Function<? super String, ? extends String> mapper,
+			Function<? super String, String> mapper,
 			StringBuilder builder) {
 		input.map(mapper).forEach(builder::append);
 	}
 
 	public static Stream<String> linesOf(String input) {
-		return Arrays.asList(input.split("\n")).stream();
+		return Arrays.stream(input.split("\n"));
 	}
 
 	public static Function<String, String> stringConstant(String postfix) {
