fcc62bfc8515c3c75c6affa21104e57e21655833
Árpád Magosányi <m4gw4s@gmail.com>
2025-09-22 10:45:04 +0200

    TestData and Stub generator framework (#77)
    * TestData and Stub generator framework
    
    ---------
    
    Co-authored-by: gbq6 <24852447+gbq6@users.noreply.github.com>

---
+++ konveyor-base/src/main/java/io/github/magwas/tooling/Generate.java
@@ -0,0 +1,27 @@
+package io.github.magwas.tooling;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.function.Supplier;
+
+public class Generate {
+
+	public static void main(String... args) throws Exception {
+		String className = args[0];
+		String targetName = className.replaceFirst("Generator$", "");
+		System.out.println("generating " + targetName);
+
+		Class klass = Class.forName(className);
+		Supplier<StringBuilder> instance = (Supplier<StringBuilder>) klass
+				.getConstructor().newInstance();
+		File file = new File("target/generated-sources/"
+				+ targetName.replaceAll("\\.", "/") + ".java");
+		new File(file.getParent()).mkdirs();
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			writer.append(instance.get());
+		}
+
+	}
+
+}
